3e9a0a8ad2966f8129856b6408f38d9480db7f06
cavalier111 <rklemchek@yahoo.com>
2017-09-05 02:36:38 -0400

    initial commit

--- apps/common/v1/news_wrapper.py
+++
@@ -1,9 +0,0 @@
-from newsapi.articles import articles
-from newsapi.sources import Sources
-
-s = Sources(API_KEY="3f7f2beb76764ae8a3e89f45954a235a")
-
-
-a = Articles(API_KEY="3f7f2beb76764ae8a3e89f45954a235a")
-
-s.get()
